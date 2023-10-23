5d16f278f49e84d23c02f5455dcdab5acbe07255
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-10-23 23:57:21 +0600

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+//Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+//Create a router object to handle routes
+const router = express.Router();
+//Get the comments data
+const comments = require('./data/comments.json');
+//Get the contacts data
+const contacts = require('./data/contacts.json');
+//Get the products data
+const products = require('./data/products.
