86f647f072c80ad213f45a57c45299f5d9dca401
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-10-23 23:48:25 +0600

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var skills = {
+        name: 'John',
+        age: 30,
+        skills: ['Javascript', 'React', 'Node', 'Vue']
+    };
+    var skills2 = {
+        name: 'John',
+        age: 30,
+        skills: ['Javascript', 'React', 'Node', 'Vue']
+    };
+}
